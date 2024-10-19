70a8539ebd227e68065a52de4ec7ae70506b064e
{Osamashehab428} <{shehabosama983@gmail.com}>
2024-10-19 04:51:46 +0300

    change

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,12 +1,13 @@
 import mysql.connector;
 
 CREATE DATABASE IF NOT EXISTS alx_book_store;
-alx_book_store = mysql.connector.connect(
+ alx_book_store = mysql.connector.connect(
   host="localhost",
   user="root@localhost",
   password="password"
 );
-
- 
+DECLARE alx_book_store HANDLER
+   FOR condition_value
+   statement
 
  
