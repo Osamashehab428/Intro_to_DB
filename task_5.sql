e9eb8dae65d6f8a63bfcc01283a8871fc0bc2291
{Osamashehab428} <{shehabosama983@gmail.com}>
2024-10-19 05:22:12 +0300

    Message

---
+++ task_5.sql
@@ -0,0 +1,6 @@
+ insert into alx_book_store.customers(customer_id,customer_name,email,address) 
+ values(1,"Cole Baidoo","cbaidoo@sandtech.com","123 Happiness Ave");
+ 
+ select * from customers;
+ 
+ 
